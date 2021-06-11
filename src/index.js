26b249c7b5426c6724cfb4f4a1b405bc34be5def
Tomasgb <tomasgomezbelcastro@gmail.com>
2021-06-10 23:25:52 -0300

    added image carrousel that changes to next image, and rotates after 5s

--- src/index.js
+++ src/index.js
@@ -6,4 +6,42 @@ menuButton.addEventListener('click', function(){
     menuNav.classList.toggle('menushow');
     menuNav.classList.toggle('menuhidd');
 
-})
+});
+
+
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+
+const nextSlide = () => {
+
+    const images = document.querySelectorAll('.slide');
+    let actual;
+
+    images.forEach(function(one){
+
+       const attr = one.getAttribute('style');
+       if(attr == 'display: block'){
+
+        const number = one.getAttribute('id');
+        if(number == 5){
+            actual = 0;
+        }else{
+        actual = parseInt(number);}
+       }
+
+       one.setAttribute('style', 'display: none');
+    })
+    console.log(actual)
+    document.getElementById(`${actual + 1}`).setAttribute('style', 'display: block');
+}
+
+nextButton.addEventListener('click', nextSlide);
+
+function runner() {
+    nextSlide();
+    setTimeout(function() {
+        runner();
+    }, 5000);
+}
+
+runner();
